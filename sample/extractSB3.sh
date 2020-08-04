6e773bfb4985cc45064cec4f87e3536ea8185487
sohta02 <sohta02@fa2.so-net.ne.jp>
2020-08-04 09:24:59 +0900

    -ドーナッツゲーム、キーボード操縦追加

---
+++ sample/extractSB3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+LIST=`find . -name "*.sb3" | tr -d ' ' | tr '\n' ' '`
+rm internalapi/* internalapi/asset/*
+for n in $LIST
+do
+  m=`echo $n | sed -e "s/\.\/\([^.]*\)\.[^.]*\.[^.]*\.[^.]*/\1/"`
+  if [ "$m" != "$n" ]; then
+    echo $m
+    unzip -o -q $n -d internalapi
+    mv internalapi/project.json internalapi/$m
+  fi
+done
+
+mv internalapi/*.wav internalapi/*.svg internalapi/*.png internalapi/asset/
+
+cp -rf internalapi /mnt/c/Users/n-tom/Desktop/touch/webSakura/tukurutch/
